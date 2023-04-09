f545c271fd6b20ceba6caec44d241437b94c1dc3
shevernitskiy <mahahuha@ya.ru>
2023-04-09 15:30:24 +0300

    fix test `GetRegions`

--- tests/get_regions.test.ts
+++ tests/get_regions.test.ts
@@ -3,7 +3,11 @@ import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 import { Cdek } from "../src/cdek.ts";
 
 Deno.test("checking code below", async () => {
-  const client = new Cdek("EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI", "PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG");
+  const client = new Cdek({
+    account: "EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI",
+    password: "PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG",
+    url_base: "https://api.edu.cdek.ru/v2",
+  });
 
   const data = await client.getRegions({ country_codes: ["TH"], size: 1 });
 
